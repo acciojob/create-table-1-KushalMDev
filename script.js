910589237ca47974b5f6a172d305ad2eac55172d
KushalMDev <105456987+KushalMDev@users.noreply.github.com>
2024-03-27 14:16:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+let count=3
 function insert_Row() {
     //Write your code here
   
-  
+  let table=document.getElementById("sampleTable")
+    let newRow=document.createElement("tr")
+    let newCell1=document.createElement("td")
+    newCell1.textContent=`Row${count} cell1`
+    let newCell2=document.createElement("td")
+    newCell2.textContent=`Row${count++} cell2`
+    
+    newRow.appendChild(newCell1)
+    newRow.appendChild(newCell2)
+    table.appendChild(newRow)
 }
